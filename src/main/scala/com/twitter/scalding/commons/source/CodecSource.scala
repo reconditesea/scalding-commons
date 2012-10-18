191592791b89bb0fc16b9a0719c096455cd184c3
Sam Ritchie <sritchie@twitter.com>
2012-10-18 14:15:05 -0700

    Add in bijection and BytesWritable conversion.

--- src/main/scala/com/twitter/scalding/commons/source/CodecSource.scala
+++ src/main/scala/com/twitter/scalding/commons/source/CodecSource.scala
@@ -24,9 +24,9 @@ import cascading.tap.hadoop.Hfs
 import cascading.tuple.Fields
 import com.twitter.chill.MeatLocker
 import com.twitter.scalding._
-import com.twitter.util.Codec
-import org.apache.hadoop.io.Writable
-import org.apache.hadoop.io.{ ByteWritable, BytesWritable, IntWritable, Text, LongWritable, NullWritable }
+import com.twitter.util.{ Bijection, Codec }
+import java.util.Arrays
+import org.apache.hadoop.io.BytesWritable
 import org.apache.hadoop.mapred.{ JobConf, OutputCollector, RecordReader }
 
 /**
@@ -34,16 +34,21 @@ import org.apache.hadoop.mapred.{ JobConf, OutputCollector, RecordReader }
  * for serialization.
  */
 
-object CodecSource {
-  def apply[T](path: String)(implicit codec: Codec[T, Array[Byte]]) = new CodecSource[T](path)
+object BytesWritableCodec extends Codec[Array[Byte], BytesWritable] {
+  override def encode(b: Array[Byte]) = new BytesWritable(b)
+  override def decode(w: BytesWritable) = Arrays.copyOfRange(w.getBytes, 0, w.getLength)
 }
 
-class CodecSource[T] private (path: String)(@transient implicit val codec: Codec[T, Array[Byte]]) extends Source {
+object CodecSource {
+  def apply[T](path: String)(implicit codec: Bijection[T, Array[Byte]]) = new CodecSource[T](path)
+}
+
+class CodecSource[T] private (path: String)(@transient implicit val codec: Bijection[T, Array[Byte]]) extends Source {
   import Dsl._
 
   val fieldSym = 'encodedBytes
 
-  val codecBox = new MeatLocker(codec)
+  val codecBox = new MeatLocker(codec andThen BytesWritableCodec)
 
   override def hdfsScheme =
     new WritableSequenceFile(new Fields(null, fieldSym.name), classOf[BytesWritable])
@@ -63,8 +68,8 @@ class CodecSource[T] private (path: String)(@transient implicit val codec: Codec
   }
 
   override def transformForRead(pipe: Pipe) =
-    pipe.map((fieldSym) -> (fieldSym)) { codecBox.get.decode(_: Array[Byte]) }
+    pipe.map((fieldSym) -> (fieldSym)) { codecBox.get.invert(_: BytesWritable) }
 
   override def transformForWrite(pipe: Pipe) =
-    pipe.mapTo((0) -> (fieldSym)) { codecBox.get.encode(_: T) }
+    pipe.mapTo((0) -> (fieldSym)) { codecBox.get.apply(_: T) }
 }
