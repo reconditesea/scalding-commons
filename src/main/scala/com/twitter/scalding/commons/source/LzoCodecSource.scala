82379a20a7e686d1f0c74d34955e0a864fd787c2
Sam Ritchie <sritchie@twitter.com>
2012-11-18 12:47:46 -0800

    Add LzoCodec trait.

--- src/main/scala/com/twitter/scalding/commons/source/LzoCodecSource.scala
+++ src/main/scala/com/twitter/scalding/commons/source/LzoCodecSource.scala
@@ -16,10 +16,7 @@ limitations under the License.
 
 package com.twitter.scalding.commons.source
 
-import cascading.pipe.Pipe
-import cascading.tuple.Fields
 import com.twitter.chill.MeatLocker
-import com.twitter.elephantbird.cascading2.scheme.LzoByteArrayScheme
 import com.twitter.scalding._
 import com.twitter.util.Bijection
 
@@ -29,23 +26,10 @@ import com.twitter.util.Bijection
  */
 
 object LzoCodecSource {
-  def apply[T](paths: String*)(implicit codec: Bijection[T, Array[Byte]]) = new LzoCodecSource[T](paths)
-}
-
-class LzoCodecSource[T] private (val hdfsPaths: Seq[String])(@transient implicit val codec: Bijection[T, Array[Byte]])
-  extends FileSource
-  with Mappable[T] {
-  import Dsl._
-  val codecBox = new MeatLocker(codec)
-
-  override val converter = Dsl.singleConverter[T]
-  override def localPath = sys.error("Local mode not yet supported.")
-
-  override def hdfsScheme = HadoopSchemeInstance(new LzoByteArrayScheme)
-
-  override def transformForRead(pipe: Pipe) =
-    pipe.map((0) -> (0)) { codecBox.get.invert(_: Array[Byte]) }
-
-  override def transformForWrite(pipe: Pipe) =
-    pipe.mapTo((0) -> (0)) { codecBox.get.apply(_: T) }
+  def apply[T](paths: String*)(implicit passedCodec: Bijection[T, Array[Byte]]) =
+    new LzoCodec[T] {
+      val hdfsPaths = paths
+      val boxed = new MeatLocker(passedCodec)
+      override def codec = boxed.get
+    }
 }
